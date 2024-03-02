c4d51d9a6d1e8ae72731e64567987e60e5018dba
He-Is-HaZaRdOuS <wither725@gmail.com>
2024-03-02 12:33:03 +0300

    Add Unix/Windows Pre-requisites -Nuke SDL2_ttf and its dependencies

--- README.md
+++ README.md
@@ -1,29 +1,108 @@
 # 415pr1
+
 Placeholder repository for CENG415's project. </br>
 https://github.com/He-Is-HaZaRdOuS/rt-rs
 
 # About
+
 The aim of this project is to build a simple ray tracer from the ground up.
 
 # How?
+
 fn main() { trace_rays(); } /s
 
 # Why Rust?
+
 ![Alt](memes/silence.jpg)
 
 # Usage
 
+### Pre-requisites (Windows)
+
+* C/C++ Toolchain (MSVC/MinGW/MSYS2)
+* SDL2
+
+#### C/C++ Toolchain
+
+* Preferably You should have
+  the [Visual Compiler](https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2022) (MSVC) by
+  installing Visual Studio 2017 or later and its C/C++ Build tools as rustup default to
+  MSVC on Windows
+* If not, You can install [MinGW](https://nuwen.net/files/mingw/mingw-18.0-without-git.exe) and copy the SDL2 lib/dll
+  files to MinGW's lib/ directory
+* Or use [MSYS2](https://www.msys2.org/) to install MinGW and copy the SDL2 lib/dll files to the lib/ directory inside
+  ucrt64
+
+### Pre-requisites (Unix)
+
+* CMake
+* C/C++ Toolchain (gcc11/g++11)
+* SDL2
+* SDL2_gfx
+
+#### CMake
+
+* Install CMake from your distribution's package manager or...
+
+```bash
+ version=3.28
+ build=1
+ limit=3.20
+ result=$(echo "$version >= $limit" | bc -l)
+ os=$([ "$result" == 1 ] && echo "linux" || echo "Linux")
+ mkdir ~/temp
+ cd ~/temp
+ wget https://cmake.org/files/v$version/cmake-$version.$build-$os-x86_64.sh
+ sudo mkdir /opt/cmake
+ sudo sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake #(Type "y" to accept the license agreement and type "n" to forego installing inside the subdirectory)
+ cmake --version #(expected output is "cmake version 3.28.1") 
+```
+
+#### C/C++ Toolchain
+
+* Install/Update gcc and g++ from your distribution's package manager
+
+#### SDL2
+
+* Debian and its derivatives (APT)
+
+````
+sudo apt-get install libsdl2-dev
+````
+
+* RHEL and its derivatives (DNF)
+
+````
+sudo dnf install SDL2-devel
+````
+
+* RHEL and its derivatives (YUM)
+
+````
+yum install SDL2-devel
+````
+
+* Or visit https://wiki.libsdl.org/SDL2/Installation to build SDL2 from source
+
+#### SDL2_gfx
+
+* Download the source from [here](https://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-1.0.4.tar.gz) and follow the build
+  instructions from [here](https://www.ferzkopp.net/Software/SDL2_gfx/Docs/html/index.html)
+
 ### #1 Install the Rust build system (Cargo)
+
 https://doc.rust-lang.org/cargo/getting-started/installation.html
 
 ### #2 Clone this repository
+
 ```
 git clone https://github.com/He-Is-HaZaRdOuS/rt-rs.git
 ```
+
 or just download as a zip and extract it
 
 ### #3 Run the program
+
 ```
 cd rt-rs && cargo run --release
 ```
-
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::mem::{size_of, size_of_val};
 use std::time::{Duration, Instant};
 
 use image::ColorType::Rgba8;
-use sdl2::{rect::Rect, render::Canvas, video::Window};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
 
 use crate::test::*;
 
@@ -59,32 +58,10 @@ pub fn sdl2() {
     //let mut image_buffer: RgbaImage = s.render();
     let mut raw_pixels: Vec<u8>;
 
-    // Create a font context
-    let ttf_context = sdl2::ttf::init().unwrap();
-    let font = ttf_context
-        .load_font("res/font/Acme 9 Regular.ttf", 10)
-        .expect("INVALID FONT PATH!"); // Specify the path to your font
-
     // Main loop
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut last_frame_time = Instant::now();
-    let mut frames = 0;
-    let mut fps_sum = 0.0;
-    let mut frame_time_sum = Duration::new(0, 0);
-    const AVERAGE_FRAMES: usize = 20; // Adjust this value to change the number of frames to average over
-    let mut average_fps: f64; // Declare average FPS outside the if block
-    let mut average_frame_time: Duration; // Declare average frame time outside the if block
-    let initial_fps_text = "Average FPS: 0.00";
-    let initial_frame_time_text = "Average Frame Time: 0";
 
-    let mut surface_fps = font
-        .render(initial_fps_text)
-        .blended(sdl2::pixels::Color::RGBA(255, 255, 255, 255)) // White text color
-        .unwrap();
-    let mut surface_frame_time = font
-        .render(initial_frame_time_text)
-        .blended(sdl2::pixels::Color::RGBA(255, 255, 255, 255)) // White text color
-        .unwrap();
     //image_buffer = s.render();
     raw_pixels = s_renderer.render().into_raw();
 
@@ -115,7 +92,7 @@ pub fn sdl2() {
             width * PixelFormatEnum::RGBA32.byte_size_per_pixel() as u32,
             PixelFormatEnum::RGBA32,
         )
-        .unwrap();
+            .unwrap();
         let texture_creator = canvas.texture_creator();
         let texture = texture_creator
             .create_texture_from_surface(surface)
@@ -136,55 +113,10 @@ pub fn sdl2() {
         let fps = 1.0 / frame_time.as_secs_f64();
         println!("FPS: {:?}, Frametime: {:?}", fps, frame_time);
 
-        /* draw avg fps and frametime in corner */
-        if DEBUG {
-            // Add FPS and frame time to the running sum
-            fps_sum += fps;
-            frame_time_sum += frame_time;
-
-            // Increment frame count
-            frames += 1;
-
-            // If we have accumulated enough frames, calculate the average
-            if frames >= AVERAGE_FRAMES {
-                average_fps = fps_sum / frames as f64;
-                average_frame_time = frame_time_sum / frames as u32;
-
-                // Reset sums and frame count
-                fps_sum = 0.0;
-                frame_time_sum = Duration::new(0, 0);
-                frames = 0;
-
-                // Create surfaces for displaying average FPS and frame time
-                let fps_text = format!("Avg FPS: {:.2}", average_fps);
-                let frame_time_text = format!("Avg Frame Time: {:?}", average_frame_time);
-                surface_fps = font
-                    .render(&fps_text)
-                    .blended(sdl2::pixels::Color::RGBA(255, 255, 255, 255)) // White text color
-                    .unwrap();
-                surface_frame_time = font
-                    .render(&frame_time_text)
-                    .blended(sdl2::pixels::Color::RGBA(255, 255, 255, 255)) // White text color
-                    .unwrap();
-            }
-
-            // Draw FPS and frame time to the corner of the window if displaying average
-            draw_text(&mut canvas, &surface_fps, 10, 10);
-            draw_text(&mut canvas, &surface_frame_time, 10, 50);
-        }
-
         // Update the window
         canvas.present();
 
         // Add a slight delay to avoid consuming too much CPU
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 240));
     }
-}
-
-fn draw_text(canvas: &mut Canvas<Window>, surface: &sdl2::surface::Surface, x: i32, y: i32) {
-    let binding = canvas.texture_creator();
-    let texture = binding.create_texture_from_surface(surface).unwrap();
-    let texture_query = texture.query();
-    let dest_rect = Rect::new(x, y, texture_query.width, texture_query.height);
-    canvas.copy(&texture, None, dest_rect).unwrap();
 }
